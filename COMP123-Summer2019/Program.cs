d305369212793577490cf429c846d61d9505407a
Thi Thu Ha Nguyen <tnguy655@my.centennialcollege.ca>
2019-07-31 22:22:01 -0400

    Add project files.

---
+++ COMP123-Summer2019/Program.cs
@@ -0,0 +1,40 @@
+﻿using COMP123_Summer2019.Models;
+using COMP123_Summer2019.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_Summer2019
+{
+   public static class Program
+    {
+        //static members 
+        public static Product product;
+        public static SplashForm splashForm;
+        public static StartForm startForm;
+        public static SelectForm selectForm;
+        public static ProductInfoForm productInfoForm;
+        public static AboutForm aboutForm;
+        public static OrderForm orderForm;
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //instantiate all forms here
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            startForm = new StartForm();
+            splashForm = new SplashForm();
+            selectForm = new SelectForm();
+            orderForm = new OrderForm();
+            productInfoForm = new ProductInfoForm();
+            aboutForm = new AboutForm();
+            product = new Product();
+            Application.Run(selectForm);
+        }
+    }
+}
---
+++ COMP123-Summer2019/Views/SelectForm.cs
@@ -0,0 +1,36 @@
+﻿using COMP123_Summer2019.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_Summer2019.Views
+{
+    public partial class SelectForm : Form
+    {
+        public SelectForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectForm_Load(object sender, EventArgs e)
+        {
+            using (var db = new DollarComputersContext())
+            {
+                db.products.Load();
+                productBindingSource.DataSource = db.products.Local.ToBindingList();
+            }
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
